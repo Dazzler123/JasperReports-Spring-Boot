15c66fc8ebec147349298f3edba7523f7a257e38
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-08-03 12:25:01 +0530

    Introduced CustomerDTO

---
+++ src/main/java/lk/dazzler/devo/java/spring/jasper/reports/dto/CustomerDTO.java
@@ -0,0 +1,14 @@
+/**
+ * #=== Created by Dazzler7Soft.Eng.Udg ===#
+ * Date : 03-Aug-2023
+ * Time : 12:23 PM
+ * Project Name : JasperReports-Spring-Boot
+ */
+package lk.dazzler.devo.java.spring.jasper.reports.dto;
+
+public class CustomerDTO {
+    private String id;
+    private String name;
+    private int age;
+    private String address;
+}
